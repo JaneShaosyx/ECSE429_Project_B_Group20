4f00de1705dd788c3ba025abba29d2c84606e9b4
Jane <yuxin.shao@mail.mcgill.ca>
2021-03-08 00:35:59 +0800

    Add some step definitions

--- src/test/java/BDDTest/MyStepdefs.java
+++ src/test/java/BDDTest/MyStepdefs.java
@@ -1,138 +1,324 @@
 package BDDTest;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 
-public class MyStepdefs {
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+public class MyStepdefs extends TestInitialization {
+
+    @Before
+    public void setUpEnvironment() {
+        environmentSetUp();
+        statusCode = 0;
+        errorMessage = "";
+        response = null;
+        originalValue = null;
+        originalTodoList = null;
+        taskList = null;
+    }
+
+    @After
+    public void restoreInitialState() {
+        checkSideEffect();
+        stopServer();
+    }
+
     @Given("the service is running")
     public void theServiceIsRunning() {
+        while (!isOnline) {
+            try {
+                environmentSetUp();
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Given("the project name with {string} is not in the system:")
     public void theProjectNameWithIsNotInTheSystem(String arg0) {
+        assertNull(findProjectByTitle(arg0));
     }
 
     @When("user requests to create a project with title {string} and description {string}")
     public void userRequestsToCreateAProjectWithTitleAndDescription(String arg0, String arg1) {
+        HttpResponse<JsonNode> res = Unirest.post("/projects").body("{\"title\":\"" + arg0 + "\", \"description\":\"" + arg1 + "\"}").asJson();
+        response = res.getBody().getObject();
+        statusCode = res.getStatus();
     }
 
     @Then("the project with title {string} and description {string} should be created:")
     public void theProjectWithTitleAndDescriptionShouldBeCreated(String arg0, String arg1) {
+        assertEquals(statusCode, 201);
+        assertEquals(response.getString("title"), arg0);
+        Unirest.delete("/projects/" + response.getInt("id")).asEmpty();
     }
 
     @Given("the project name with {string} , completed status {string} is registered in the system:")
     public void theProjectNameWithCompletedStatusIsRegisteredInTheSystem(String arg0, String arg1) {
+        while (findProjectByTitle(arg0) == null) {
+            try {
+                Unirest.post("/projects").body("{\"title\":\"" + arg0 + "\", \"completed\":" + arg1 + "}").asJson();
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        assertNotNull(findProjectByTitle(arg0));
     }
 
     @When("user requests to change the complete status of the project with title {string}  to {string}")
     public void userRequestsToChangeTheCompleteStatusOfTheProjectWithTitleTo(String arg0, String arg1) {
+        int id = findProjectByTitle(arg0).getInt("id");
+        HttpResponse<JsonNode> res = Unirest.post("/projects/" + id).body("{\"completed\":" + arg1 + "}").asJson();
+        response = res.getBody().getObject();
+        statusCode = res.getStatus();
     }
 
     @Then("the complete status of the project with title {string} will be set to {string}")
     public void theCompleteStatusOfTheProjectWithTitleWillBeSetTo(String arg0, String arg1) {
+        assertEquals(statusCode, 200);
+        assertEquals(response.getString("completed"), arg1);
+        Unirest.delete("/projects/" + response.getInt("id")).asEmpty();
     }
 
     @When("user requests to create a project with title {string} and active status {string}")
     public void userRequestsToCreateAProjectWithTitleAndActiveStatus(String arg0, String arg1) {
+        HttpResponse<JsonNode> res = Unirest.post("/projects").body("{\"title\":\"" + arg0 + "\", \"active\":" + arg1 + "}").asJson();
+        response = res.getBody().getObject();
+        statusCode = res.getStatus();
     }
 
     @Then("the system should output an error code of wrong data type of active status: {string}")
     public void theSystemShouldOutputAnErrorCodeOfWrongDataTypeOfActiveStatus(String arg0) {
+        assertEquals(statusCode, Integer.parseInt(arg0));
     }
 
     @Given("the todo name with {string} and description {string} is not in the system:")
     public void theTodoNameWithAndDescriptionIsNotInTheSystem(String arg0, String arg1) {
+        assertNull(findTodoByTitle(arg0));
     }
 
     @When("user requests to create a todo with title {string} and description {string}")
     public void userRequestsToCreateATodoWithTitleAndDescription(String arg0, String arg1) {
+        HttpResponse<JsonNode> res = Unirest.post("/todos").body("{\"title\":\"" + arg0 + "\", \"description\":\"" + arg1 + "\"}").asJson();
+        response = res.getBody().getObject();
+        statusCode = res.getStatus();
     }
 
     @Then("the todo task with title {string} and description {string} should be created:")
     public void theTodoTaskWithTitleAndDescriptionShouldBeCreated(String arg0, String arg1) {
+        assertEquals(statusCode, 201);
+        assertEquals(response.getString("title"), arg0);
+        assertEquals(response.getString("description"), arg1);
+        Unirest.delete("/todos/" + response.getInt("id")).asEmpty();
     }
 
     @Given("the todo task name with {string} ,  done status {string} is registered in the system:")
     public void theTodoTaskNameWithDoneStatusIsRegisteredInTheSystem(String arg0, String arg1) {
+        while (findTodoByTitle(arg0) == null) {
+            try {
+                Unirest.post("/todos").body("{\"title\":\"" + arg0 + "\", \"doneStatus\":" + arg1 + "}").asJson();
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        assertNotNull(findTodoByTitle(arg0));
     }
 
     @When("user requests to change the  done status of the todo task with title {string}  to {string}")
     public void userRequestsToChangeTheDoneStatusOfTheTodoTaskWithTitleTo(String arg0, String arg1) {
+        int id = findProjectByTitle(arg0).getInt("id");
+        HttpResponse<JsonNode> res = Unirest.post("/todos/" + id).body("{\"doneStatus\":" + arg1 + "}").asJson();
+        response = res.getBody().getObject();
+        statusCode = res.getStatus();
     }
 
     @Then("the  done status of the todo task with title {string} will be set to {string}")
     public void theDoneStatusOfTheTodoTaskWithTitleWillBeSetTo(String arg0, String arg1) {
+        assertEquals(statusCode, 200);
+        assertEquals(response.getString("doneStatus"), arg1);
+        Unirest.delete("/todos/" + response.getInt("id")).asEmpty();
     }
 
     @Given("the todo name with {string} and done status {string} is not in the system:")
     public void theTodoNameWithAndDoneStatusIsNotInTheSystem(String arg0, String arg1) {
+        assertNull(findTodoByTitle(arg0));
     }
 
     @When("user requests to create a todo with title {string} and done status {string}")
     public void userRequestsToCreateATodoWithTitleAndDoneStatus(String arg0, String arg1) {
+        HttpResponse<JsonNode> res = Unirest.post("/todos").body("{\"title\":\"" + arg0 + "\", \"doneStatus\":" + arg1 + "}").asJson();
+        response = res.getBody().getObject();
+        statusCode = res.getStatus();
     }
 
     @Then("the system should output an error code of wrong data type of doneStatus: {string}")
     public void theSystemShouldOutputAnErrorCodeOfWrongDataTypeOfDoneStatus(String arg0) {
+        assertEquals(statusCode, Integer.parseInt(arg0));
     }
 
     @Given("the project name with {string} and todo name with {string} is in the system:")
     public void theProjectNameWithAndTodoNameWithIsInTheSystem(String arg0, String arg1) {
+        while (findTodoByTitle(arg1) == null) {
+            try {
+                Unirest.post("/todos").body("{\"title\":\"" + arg0 + "\"}").asJson();
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        while (findProjectByTitle(arg0) == null) {
+            try {
+                Unirest.post("/projects").body("{\"title\":\"" + arg0 + "\"}").asJson();
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        assertNotNull(findProjectByTitle(arg0));
+        assertNotNull(findTodoByTitle(arg1));
     }
 
     @When("user requests to add a todo task with title {string} to the project with title {string}")
     public void userRequestsToAddATodoTaskWithTitleToTheProjectWithTitle(String arg0, String arg1) {
+        int id_1 = findTodoByTitle(arg0).getInt("id");
+        int id_2 = findProjectByTitle(arg1).getInt("id");
+        HttpResponse<JsonNode> res = Unirest.post("/todos/" + id_1 + "/tasksof").body("{\"id\":\"" + id_2 + "\"}").asJson();
+        statusCode = res.getStatus();
     }
 
     @Then("the a todo task with title {string} will be add to the tasks list of project with title {string}")
     public void theATodoTaskWithTitleWillBeAddToTheTasksListOfProjectWithTitle(String arg0, String arg1) {
+        assertEquals(statusCode, 201);
+        int id_1 = findTodoByTitle(arg0).getInt("id");
+        int id_2 = findProjectByTitle(arg1).getInt("id");
+        JSONArray jsonArray = findTodoByTitle(arg0).getJSONArray("tasksof");
+        int find_id_2 = 0;
+        for (Object proj : jsonArray) {
+            JSONObject project = (JSONObject) proj;
+            if (project.getInt("id") == id_2) {
+                find_id_2 = id_2;
+            }
+        }
+        assertEquals(find_id_2, id_2);
+
+        JSONArray jsonArray_2 = findProjectByTitle(arg1).getJSONArray("tasks");
+        int find_id_1 = 0;
+        for (Object todo : jsonArray_2) {
+            JSONObject todo_task = (JSONObject) todo;
+            if (todo_task.getInt("id") == id_1) {
+                find_id_1 = id_1;
+            }
+        }
+        assertEquals(find_id_1, id_1);
+
     }
 
     @Given("the project name with {string} is in the system")
     public void theProjectNameWithIsInTheSystem(String arg0) {
+        while (findProjectByTitle(arg0) == null) {
+            try {
+                Unirest.post("/projects").body("{\"title\":\"" + arg0 + "\"}").asJson();
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        assertNotNull(findProjectByTitle(arg0));
     }
 
     @And("the todo task name with {string} is not in the system:")
     public void theTodoTaskNameWithIsNotInTheSystem(String arg0) {
+        assertNull(findTodoByTitle(arg0));
     }
-    
+
 
     @And("the todo name with {string} and description {string} is in the system")
     public void theTodoNameWithAndDescriptionIsInTheSystem(String arg0, String arg1) {
+        while (findTodoByTitle(arg0) == null) {
+            try {
+                Unirest.post("/todos").body("{\"title\":\"" + arg0 + "\"}").asJson();
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        assertNotNull(findTodoByTitle(arg0));
     }
 
     @When("user requests to add a todo task with title {string} and description {string} to the project with title {string}")
     public void userRequestsToAddATodoTaskWithTitleAndDescriptionToTheProjectWithTitle(String arg0, String arg1, String arg2) {
+        int id_1 = findTodoByTitle(arg0).getInt("id");
+        int id_2 = findProjectByTitle(arg1).getInt("id");
+        HttpResponse<JsonNode> res = Unirest.post("/todos/" + id_1 + "/tasksof").body("{\"id\":\"" + id_2 + "\"}").asJson();
+        statusCode = res.getStatus();
     }
 
     @Then("the system should output an error code of add todo task with non-existent project id: {string}")
     public void theSystemShouldOutputAnErrorCodeOfAddTodoTaskWithNonExistentProjectId(String arg0) {
+        assertEquals(statusCode, Integer.parseInt(arg0));
     }
 
     @Given("the category name with {string} is not in the system")
     public void theCategoryNameWithIsNotInTheSystem(String arg0) {
+        assertNull(findCategoryByTitle(arg0));
     }
 
     @When("user requests to create a category with title {string} and description of {string}")
     public void userRequestsToCreateACategoryWithTitleAndDescriptionOf(String arg0, String arg1) {
+        HttpResponse<JsonNode> res = Unirest.post("/categories").body("{\"title\":\"" + arg0 + "\", \"description\":\"" + arg1 + "\"}").asJson();
+        response = res.getBody().getObject();
+        statusCode = res.getStatus();
     }
 
     @Then("the category with title {string} and description {string} should be created")
     public void theCategoryWithTitleAndDescriptionShouldBeCreated(String arg0, String arg1) {
+        assertEquals(statusCode, 201);
+        assertEquals(response.getString("title"), arg0);
+        assertEquals(response.getString("description"), arg1);
+        Unirest.delete("/categories/" + response.getInt("id")).asEmpty();
     }
 
     @When("user requests to create a category with title {string}")
     public void userRequestsToCreateACategoryWithTitle(String arg0) {
+        HttpResponse<JsonNode> res = Unirest.post("/categories").body("{\"title\":\"" + arg0 + "\"}").asJson();
+        response = res.getBody().getObject();
+        statusCode = res.getStatus();
     }
 
     @Then("the category with title {string} should be created")
     public void theCategoryWithTitleShouldBeCreated(String arg0) {
+        assertEquals(statusCode, 201);
+        assertEquals(response.getString("title"), arg0);
+        Unirest.delete("/categories/" + response.getInt("id")).asEmpty();
     }
 
     @Given("the category name description of {string} is not in the system")
     public void theCategoryNameDescriptionOfIsNotInTheSystem(String arg0) {
+        JSONObject response = Unirest.get("/categories?description=" + "\"" + arg0 + "\"").asJson().getBody().getObject();
+        ArrayList<JSONObject> arr = new ArrayList<JSONObject>();
+        for (Object cate : response.getJSONArray("categories")) {
+            JSONObject category = (JSONObject) cate;
+            if (category.getString("description").equals(arg0)) {
+                arr.add(category);
+            }
+        }
+        assertEquals(arr.size(), 0);
     }
 
     @When("user requests to create a category with description {string}")
---
+++ src/test/java/BDDTest/TestInitialization.java
@@ -0,0 +1,104 @@
+package BDDTest;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+import org.junit.After;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.*;
+
+public class TestInitialization {
+    private static Process serverProcess;
+    private static final String URL = "http://localhost:4567";
+    static String errorMessage;
+    static int statusCode;
+    static JSONObject originalValue;
+    static JSONObject response;
+    static JSONObject originalTodoList;
+    static JSONArray taskList;
+    public static boolean isShutdown = false;
+    public static boolean isOnline = false;
+
+
+    public static void environmentSetUp() {
+        Unirest.config().defaultBaseUrl(URL);
+        startServerAPI();
+    }
+
+    public static void startServerAPI() {
+        try {
+            ProcessBuilder inputS = new ProcessBuilder("java", "-jar", "runTodoManagerRestAPI-1.5.5.jar");
+            serverProcess = inputS.start();
+            final InputStream is = serverProcess.getInputStream();
+            final InputStreamReader inputSR = new InputStreamReader(is);
+            final BufferedReader output = new BufferedReader(inputSR);
+            while (true) {
+                String in = output.readLine();
+                if (in.contains("Running on 4567")) {
+                    isOnline = true;
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void stopServer() {
+        serverProcess.destroy();
+    }
+
+    public void checkSideEffect() {
+        if (isShutdown) {
+            isShutdown = !isShutdown;
+            return;
+        }
+        String title = Unirest.get("/todos").asJson().getBody().getObject().getJSONArray("todos").getJSONObject(0).getString("title");
+        assertTrue(title.equals("scan paperwork") || title.equals("file paperwork"));
+        assertEquals("Office Work", Unirest.get("/projects").asJson().getBody().getObject().getJSONArray("projects").getJSONObject(0).getString("title"));
+        HttpResponse<JsonNode> response = Unirest.get("/categories").asJson();
+        String title1 = response.getBody().getObject().getJSONArray("categories").getJSONObject(0).getString("title");
+        assertTrue(title1.equals("Home") || title1.equals("Office"));
+    }
+
+    public static JSONObject findProjectByTitle(String projectName) {
+        JSONObject response = Unirest.get("/projects?title="  + "\""+projectName+"\"").asJson().getBody().getObject();
+        for (Object proj : response.getJSONArray("projects")) {
+            JSONObject project = (JSONObject) proj;
+            if (project.getString("title").equals(projectName)) {
+                return project;
+            }
+        }
+        return null;
+    }
+
+    public static JSONObject findTodoByTitle(String todoTitle) {
+        JSONObject response = Unirest.get("/todos?title="  + "\""+todoTitle+"\"").asJson().getBody().getObject();
+        for (Object todo : response.getJSONArray("todos")) {
+            JSONObject todo_task = (JSONObject) todo;
+            if (todo_task.getString("title").equals(todoTitle)) {
+                return todo_task;
+            }
+        }
+        return null;
+    }
+
+    public static JSONObject findCategoryByTitle(String categoryTitle) {
+        JSONObject response = Unirest.get("/categories?title=" + "\""+categoryTitle+"\"").asJson().getBody().getObject();
+        for (Object cate : response.getJSONArray("categories")) {
+            JSONObject category = (JSONObject) cate;
+            if (category.getString("title").equals(categoryTitle)) {
+                return category;
+            }
+        }
+        return null;
+    }
+
+
+}
